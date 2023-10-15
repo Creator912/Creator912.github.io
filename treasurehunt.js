1af1efc6891a1e005dc1470c3fa089ddc23995e2
Creator912 <148017039+Creator912@users.noreply.github.com>
2023-10-15 16:31:47 -0700

    Add files via upload

---
+++ treasurehunt.js
@@ -0,0 +1,21 @@
+var treasurenumber = Math.floor(Math.random() * 9)
+var bombnumber = Math.floor(Math.random() * 9)
+// const treasure = () => {
+//     return alert('Gold!')
+//   }
+  alert("hello")
+const changeToEmoji = (treasure) => {
+    if(treasure === treasurenumber) {
+        document.getElementById(treasurenumber).innerHTML = "🎁"
+    }
+    else if(treasure === bombnumber){
+        document.getElementById(bombnumber).innerHTML = "💣"
+    }
+    else {
+        document.getElementById(treasure).innerHTML = "🌈"
+    }
+    
+  }
+
+
+//   var bomb
